05931eee70eabc63268c99dd869b88765a8577bd
Julien Coulombe <jcoulombemorency@gmail.com>
2023-05-01 11:04:48 -0400

    notes de cours fichier en bash

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# if [[ -e $1 ]]    # Fonctionne comme la ligne du dessous. 
+
+                    # -f permet de vérifier si c'est un fichier.
+                    # -d permet de vérifier si c'est un dossier. 
+if test -e $1       # -e permet de vérifier si le fichier existe. 
+then
+    echo "Le fichier existe!!"
+else
+    echo "Le fichier n'existe pas."
+fi
+
+
