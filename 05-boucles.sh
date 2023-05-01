91003919ff2cafbdf22acdd3974a445ded71346b
Julien Coulombe <jcoulombemorency@gmail.com>
2023-05-01 10:43:56 -0400

    notes de cours boucles.

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))     # Boucle for standard. Syntax comme en C++
+do
+    echo $i
+done
+
+liste=`ls`                # Le résultat de l'éxécution de la commande est placé dans la variable liste 
+
+for fichier in $liste     # Boucle foreach pour naviguer au travers du résultat. 
+do
+    echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Entrez une lettre : " resultat
+done
