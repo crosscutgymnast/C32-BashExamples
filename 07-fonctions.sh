11562d08943202786136768b9ed9ab92cee3401a
Julien Coulombe <jcoulombemorency@gmail.com>
2023-05-01 10:56:37 -0400

    note de cours fonction

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+maFonction() {
+    echo "param : " $1
+}
+
+# $0 retourne le nom de la fonction
+# $# Retourne le nombre de variables entrer en paramêtres.
+# $1, $2, $3 On accède au paramètres comme n'importe quelle autre variables. 
+
+maFonction "Salut"
+maFonction "Allo"
